9f3d5dc60310eb8de49a195085167fc5565a75c0
Princeton Rose <decoyzpr@gmail.com>
2019-03-14 02:26:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,15 @@ require 'pry'
 
 class Song
 
-@@count = 0
+  @@count = 0
+  @@genres = []
+  @@artists = []
 
   def initialized(name, artist, genre)
     @name = name
     @artist = artist
-    @genre = genre
+    @@artists << @artis
+    @@genres << @genre
     @@count += 1
   end
 
